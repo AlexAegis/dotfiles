27aecdc54385875edeef33ae503218a94d3fe86e
Sándor Gyori <sandor.gyori@ericsson.com>
2020-02-14 14:01:14 +0100

    disabled nvm

--- modules/zsh/home-zsh/.config/zsh/.zshrc
+++ modules/zsh/home-zsh/.config/zsh/.zshrc
@@ -23,7 +23,7 @@ echo Starting zsh
 # TODO: history plugin https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/history.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
+[[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
 
 ## Plugin configurations
 
--- modules/zsh/home-zsh/.rc/auto-nvm.zsh
+++ modules/zsh/home-zsh/.rc/auto-nvm.zsh
@@ -2,24 +2,26 @@
 # https://github.com/nvm-sh/nvm#automatically-call-nvm-use
 
 # place this after nvm initialization!
-autoload -U add-zsh-hook
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-  	echo $nvmrc_path
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+# TODO: Extremely slow on notebook, replace it with something, or nvm altogether
+# autoload -U add-zsh-hook
+# load-nvmrc() {
+#   local node_version="$(nvm version)"
+#   local nvmrc_path="$(nvm_find_nvmrc)"
+#
+#   if [ -n "$nvmrc_path" ]; then
+#   	echo $nvmrc_path
+#     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+#
+#     if [ "$nvmrc_node_version" = "N/A" ]; then
+#       nvm install
+#     elif [ "$nvmrc_node_version" != "$node_version" ]; then
+#       nvm use
+#     fi
+#   elif [ "$node_version" != "$(nvm version default)" ]; then
+#     echo "Reverting to nvm default version"
+#     nvm use default
+#   fi
+# }
+# add-zsh-hook chpwd load-nvmrc
+# load-nvmrc
