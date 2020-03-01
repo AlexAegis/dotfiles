599b3e1e605b76808f522d0c0085feb93436c5c2
AlexAegis <alexaegis@gmail.com>
2020-03-01 17:45:57 +0100

    prepare

--- dot.sh
+++ dot.sh
@@ -56,7 +56,8 @@
 # opens up help
 
 # dot -f --force
-# by default dot compares a tarhash of the last installed module, and only reinstalls if it changed
+# by default dot compares a tarhash of the last installed module, and only
+# reinstalls if it changed
 # if forced however it could skip this test.
 # It always updates the hash regardless of forcing or not
 # dont forget to ignore the hashfile itself from the hashing
@@ -69,7 +70,6 @@ C_RESET='\033[0m'
 C_BLUE='\033[0;34m'
 C_CYAN='\033[0;36m'
 C_RED='\033[0;31m'
-C_WHITE='\033[0;37m'
 C_YELLOW='\033[0;33m'
 
 show_help() {
@@ -81,9 +81,10 @@ is_installed() {
 }
 
 # Reset all variables that might be set
-modules_selected=
+modules_selected=""
+resolved=""
+final_module_list=""
 config=0
-# preset=
 force=0
 modules_folder="$HOME/.dotfiles/modules" # TODO: use relative to script
 presets_folder="$HOME/.dotfiles/presets" # TODO: use relative to script
@@ -93,10 +94,16 @@ dependenciesfilename=".dependencies"
 tagsfilename=".tags"
 verbose=0 # Print more
 dry=0     # When set, no installation will be done
-
+# TODO: Only needed when printing and using whiptail. Lazy load it.
 all_modules=$(find "$modules_folder/" -maxdepth 1 -mindepth 1 -printf "%f\n")
 all_presets=$(find "$presets_folder/" -maxdepth 1 -mindepth 1 \
 	-name '*.preset' -printf "%f\n" | sed 's/.preset//')
+all_tags=$(find "$modules_folder"/*/ -maxdepth 1 -mindepth 1 -name '.tags' \
+	-exec cat {} + | grep "^[^#;]" | sort | uniq)
+
+# Package manager availablity
+has_pacman=$(is_installed pacman)
+has_apt=$(is_installed apt)
 
 while :; do
 	# echo "Evaluating $1"
@@ -105,11 +112,26 @@ while :; do
 		show_help
 		exit
 		;;
+	-la | --list-all)
+		printf "${C_BLUE}All available modules:${C_RESET}\n%s\n" "$all_modules"
+		printf "${C_BLUE}All available presets:${C_RESET}\n%s\n" "$all_presets"
+		printf "${C_BLUE}All available tags:${C_RESET}\n%s\n" "$all_tags"
+		exit
+		;;
+	-lm | --list-modules)
+		printf "${C_BLUE}All available modules:${C_RESET}\n%s\n" "$all_modules"
+		exit
+		;;
+	-lp | --list-presets)
+		printf "${C_BLUE}All available presets:${C_RESET}\n%s\n" "$all_presets"
+		exit
+		;;
+	-lt | --list-tags)
+		printf "${C_BLUE}All available tags:${C_RESET}\n%s\n" "$all_tags"
+		exit
+		;;
 	-v | --verbose) # Verbose printing TODO: Pass to the dotmodules
 		verbose=1
-		;;
-	-c | --config) # Interactive customization installable modules
-		config=1
 		;;
 	-d | --dry) #  customize installable modules
 		dry=1
@@ -117,54 +139,32 @@ while :; do
 	-f | --force) # force installation
 		force=1
 		;;
-	-p | --preset) # Takes an option argument, ensuring it has been specified.
-		if [ -n "$2" ]; then
-			shift
-			modules_selected=$*
-		else
-			modules_selected=$(whiptail --title "Select" \
-				--checklist "Space changes selection, enter approves" \
-				0 0 0 zsh zsh ON fish fish OFF 3>&1 1>&2 2>&3 3>&-)
-		fi
-		;;
-	i | install) # Installs modules specified after it
-		if [ -n "$2" ]; then
-			shift
-			modules_selected=$*
-		else
-			modules_selected=$(whiptail --title "Select" \
-				--checklist "Space changes selection, enter approves" \
-				0 0 0 zsh zsh ON fish fish OFF 3>&1 1>&2 2>&3 3>&-)
-		fi
-		;;
-	--) # End of all options.
-		shift
-		break
+	-c | --config | --custom) # Ask for everything
+		config=1
 		;;
 	-?*)
 		printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
 		;;
-	*) # Default case: If no more options then break out of the loop.
-		break ;;
+	*) # Installs modules specified after it
+		if [ -n "$1" ]; then
+			modules_selected=$*
+		fi
+		break
+		;;
 	esac
 
 	shift
 done
 
-[ $verbose = 1 ] && echo "$all_modules"
-[ $verbose = 1 ] && echo "$all_presets"
-
-# Package manager availablity
-has_pacman=$(is_installed pacman)
-has_apt=$(is_installed apt)
-
-[ $verbose = 1 ] && echo has_pacman "$has_pacman"
-[ $verbose = 1 ] && echo has_apt "$has_apt"
-
-if [ -z "$modules_selected" ]; then
-	echo 'Nothing to install'
-	exit
+if [ $verbose = 1 ]; then
+	echo "has_pacman? $has_pacman"
+	echo "has_apt? $has_apt"
 fi
+
+trim_around() {
+	# removes the first and last characters from every line
+	rev | cut -c2- | rev | cut -c2-
+}
 
 has_tag() {
 	# Returns every dotmodule that contains any of the tags
@@ -188,12 +188,10 @@ get_dependencies() {
 	fi
 }
 
-resolved=""
-
 install_whatever() {
 	while :; do
 		if [ "$1" ]; then
-			[ $verbose = 1 ] && echo "    Trying to install $1..."
+			[ $verbose = 1 ] && echo "Trying to install $1..."
 			if [ "$(echo "$resolved" | grep -w "$1")" = "" ]; then
 				resolved="$resolved $1"
 				case "$1" in
@@ -208,12 +206,13 @@ install_whatever() {
 				*) # modules
 					# shellcheck disable=SC2046
 					install_whatever $(get_dependencies "$1")
-					install_module "$1"
+					final_module_list="$final_module_list $1"
+					# install_module "$1"
 					;;
 				esac
-				[ $verbose = 1 ] && echo "    done."
+				[ $verbose = 1 ] && echo "...done installing $1"
 			else
-				[ $verbose = 1 ] && echo "    Already resolved."
+				[ $verbose = 1 ] && echo "...already resolved $1"
 			fi
 			shift
 		else
@@ -223,65 +222,87 @@ install_whatever() {
 }
 
 install_module() {
+	while :; do
+		if [ "$1" ]; then
 
-	module=$1
-	shift
+			if [ ! -d "$modules_folder/$1/" ]; then
+				echo "Module $1 not found. Skipping"
+				return 1
+			fi
 
-	if [ ! -d "$modules_folder/$module/" ]; then
-		echo "Module $module not found. Skipping"
-		return 1
-	fi
+			# cd to dotmodule just in case a dotmodule
+			# is not suited for installation outside of it
+			cd "${0%/*}" || return
+			echo "${C_BLUE}Installing $1$C_RESET"
 
-	# cd to dotmodule just in case a dotmodule
-	# is not suited for installation outside of it
-	cd "${0%/*}" || return
-	echo "${C_BLUE}Installing $module$C_RESET"
+			# Only calculate the hashes if we going to use it
+			if [ "$force" = 0 ]; then
+				old_hash=$(cat "$modules_folder/$1/$hashfilename" 2>/dev/null)
+				new_hash=$(tar --absolute-names \
+					--exclude="$modules_folder/$1/$hashfilename" \
+					-c "$modules_folder/$1" | sha1sum)
+				[ $verbose = 1 ] && printf "$C_RED%s\n%s\n$C_RESET" \
+					"$old_hash" \
+					"$new_hash"
+			fi
 
-	# Only calculate the hashes if we going to use it
-	if [ "$force" = 0 ]; then
-		old_hash=$(cat "$modules_folder/$module/$hashfilename" 2>/dev/null)
-		new_hash=$(tar --absolute-names \
-			--exclude="$modules_folder/$module/$hashfilename" \
-			-c "$modules_folder/$module" | sha1sum)
-		[ $verbose = 1 ] && printf "$C_RED%s\n%s\n$C_RESET" "$old_hash" "$new_hash"
-	fi
+			if
+				[ "$force" = 1 ] || [ "$old_hash" != "$new_hash" ]
+			then
+				echo "${C_RED}Applying dotmodule $1$C_RESET"
+				if [ "$has_apt" ] &&
+					[ -f "$modules_folder/$1/install.apt.sh" ]; then
+					# shellcheck disable=SC2091
+					[ $dry != 1 ] && $("$modules_folder/$1/install.apt.sh")
+				fi
+				if [ "$has_pacman" ] &&
+					[ -f "$modules_folder/$1/install.pacman.sh" ]; then
+					# shellcheck disable=SC2091
+					[ $dry != 1 ] && $("$modules_folder/$1/install.pacman.sh")
+				fi
+				if [ -f "$modules_folder/$1/install.sudo.sh" ]; then
+					# shellcheck disable=SC2091
+					[ $dry != 1 ] && $("$modules_folder/$1/install.sudo.sh")
+				fi
+				if [ -f "$modules_folder/$1/install.sh" ]; then
+					# shellcheck disable=SC2091
+					[ $dry != 1 ] && $("$modules_folder/$1/install.sh")
+				fi
 
-	if
-		[ "$force" = 1 ] || [ "$old_hash" != "$new_hash" ]
-	then
-		echo "${C_RED}Applying dotmodule $module$C_RESET"
-		if [ "$has_apt" ] &&
-			[ -f "$modules_folder/$module/install.apt.sh" ]; then
-			# shellcheck disable=SC2091
-			[ $dry != 1 ] && $("$modules_folder/$module/install.apt.sh")
+				# Calculate fresh hash (always if not dryrunning)
+				[ $dry != 1 ] &&
+					tar --absolute-names \
+						--exclude="$modules_folder/$1/$hashfilename" \
+						-c "$modules_folder/$1" |
+					sha1sum >"$modules_folder/$1/$hashfilename"
+			else
+				echo "$C_YELLOW! $1 is already installed and no changes" \
+					"are detected$C_RESET"
+			fi
+			shift
+		else
+			break
 		fi
-		if [ "$has_pacman" ] &&
-			[ -f "$modules_folder/$module/install.pacman.sh" ]; then
-			# shellcheck disable=SC2091
-			[ $dry != 1 ] && $("$modules_folder/$module/install.pacman.sh")
-		fi
-		if [ -f "$modules_folder/$module/install.sudo.sh" ]; then
-			# shellcheck disable=SC2091
-			[ $dry != 1 ] && $("$modules_folder/$module/install.sudo.sh")
-		fi
-		if [ -f "$modules_folder/$module/install.sh" ]; then
-			# shellcheck disable=SC2091
-			[ $dry != 1 ] && $("$modules_folder/$module/install.sh")
-		fi
-
-		# Calculate fresh hash (always if not dryrunning)
-		[ $dry != 1 ] &&
-			tar --absolute-names \
-				--exclude="$modules_folder/$module/$hashfilename" \
-				-c "$modules_folder/$module" |
-			sha1sum >"$modules_folder/$module/$hashfilename"
-	else
-		echo "$C_YELLOW! $module is already installed and no changes are detected$C_RESET"
-	fi
-
+	done
 }
 
 # Actual installation process
 # This will install a non-existent module that depends on the selected modules
+
+if [ -z "$modules_selected" ] || [ "$config" = 1 ]; then
+	modules_selected=$(whiptail --title "Select modules to install" \
+		--checklist "Space changes selection, enter approves" \
+		0 0 0 zsh zsh ON fish fish OFF 3>&1 1>&2 2>&3 3>&- |
+		sed 's/ /\n/g' |
+		trim_around)
+
+fi
+
 # shellcheck disable=SC2086
 install_whatever $modules_selected
+
+[ $verbose = 1 ] && printf "${C_CYAN}Going to install:${C_RESET}\n%s\n" \
+	"$final_module_list"
+
+# shellcheck disable=SC2086
+install_module $final_module_list
