875af3963ede4505a8b2b06833658fb23089306d
Sándor Gyori <sandor.gyori@ericsson.com>
2020-03-02 10:32:12 +0100

    no symlinks

--- dotlink.sh
+++ dotlink.sh
@@ -6,11 +6,4 @@ DOT_PATH=$(
 )
 
 # Using a symlink to make dot available without modifying the PATH
-# But only when using sudo
-
-if [ "$SUDO_USER" ]; then
-	ln -sf "$DOT_PATH/src/dot.sh" "/usr/local/bin/dot"
-fi
-
-# If sourced, will expand the path with itself
-export PATH="$PATH:$DOT_PATH/"
+ln -sf "$DOT_PATH/dot.sh" "/usr/local/bin/dot"
