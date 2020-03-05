a835fefa57524e693a4f19c585ccee14a2212d14
Sándor Gyori <sandor.gyori@ericsson.com>
2020-03-05 09:37:00 +0100

    7z apt

---
+++ modules/7z/install.apt.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apt install p7zip-full p7zip-rar
--- modules/doom/install.sh
+++ modules/doom/install.sh
@@ -2,6 +2,6 @@
 
 stow -t "$HOME" -d "${0%/*}" .doom
 
-git submodule update
+git submodule update --init --recursive
 
 find "${0%/*}" -regex '.*\.7z' -exec 7z x -y -o"$HOME/.config/gzdoom/wad" {} \;
