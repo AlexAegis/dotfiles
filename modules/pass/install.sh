2d204298266b4bc745788a3619693a926b280fe3
AlexAegis <alexaegis@gmail.com>
2020-03-05 23:43:26 +0100

    presets

--- modules/pass/install.sh
+++ modules/pass/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-git init --bare "$HOME/.password-store"
+# TODO: Decide if needed
+# git init --bare "$HOME/.password-store"
 
 git config --global credential.helper /usr/bin/pass-git-helper
 git config --global credential.useHttpPath true
+
+pass init "$(gpg --list-secret-keys | grep "^      " | tail -1)"
