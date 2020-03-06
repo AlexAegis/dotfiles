b61cdb8b201d1ae747b81ab9e1db9f5a44bc2a02
AlexAegis <alexaegis@gmail.com>
2020-03-06 01:47:59 +0100

    reset

---
+++ modules/pass/reset.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# ! Must be run as the user and this is the simplest way to run an entire script
+# ! as another user without messing everything up
+mkdir -p "$HOME/.password-store"
+
+git config --global credential.helper /usr/bin/pass-git-helper
+git config --global credential.useHttpPath true
+
+if [ ! -e "$HOME/.gnupg" ]; then
+	# shellcheck disable=SC1090
+	. "$HOME/bin/gengpg"
+fi
+
+pass init "$(gpg --list-secret-keys |
+	grep "^      " |
+	tail -1 |
+	sed 's/^      //')"
+
+pass insert dev/github
