b1b69d75bd70399c4067565dbcf920bffca9dcf6
AlexAegis <alexaegis@gmail.com>
2020-04-06 02:08:12 +0200

    inotify symlink watcher

---
+++ modules/inotify/readme.md
@@ -0,0 +1,5 @@
+# [inotify](https://en.wikipedia.org/wiki/Inotify)
+
+> [Arch Wiki][wiki]
+
+[wiki]: https://wiki.archlinux.org/index.php/Autostarting#On_filesystem_events
--- modules/systemd/1.user.systemctl.sh
+++ modules/systemd/1.user.systemctl.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-systemctl enable --user --now daemon-reload.service
-systemctl enable --user --now user-services.path
+systemctl enable --user --now user-service-watcher.service
 # Don't put --now here, can't enable without instance name.
 # And they will be only started by other services
+systemctl enable --user daemon-reload.service
 systemctl enable --user notify-low@.service
 systemctl enable --user notify-normal@.service
 systemctl enable --user notify-critical@.service
