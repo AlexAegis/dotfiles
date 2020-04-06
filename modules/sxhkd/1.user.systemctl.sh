7a8b1cc34f1c6a6d687f0bb573b6dddc6f5e63b9
AlexAegis <alexaegis@gmail.com>
2020-04-06 02:49:03 +0200

    sxhkd new watcher

--- modules/sxhkd/1.user.systemctl.sh
+++ modules/sxhkd/1.user.systemctl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 systemctl --user enable --now sxhkd.service
+systemctl --user enable --now sxhkd-watcher.service
 # Disabled in favour of the entr watcher, see readme for why
 # systemctl --user enable --now sxhkd.path
