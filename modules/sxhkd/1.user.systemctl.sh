5c87a9992c2ea13db2cdee387258af67a031ac50
AlexAegis <alexaegis@gmail.com>
2020-04-09 04:23:33 +0200

    comment removed

--- modules/sxhkd/1.user.systemctl.sh
+++ modules/sxhkd/1.user.systemctl.sh
@@ -2,5 +2,3 @@
 
 systemctl --user enable --now sxhkd.service
 systemctl --user enable --now sxhkd-watcher.service
-# Disabled in favour of the entr watcher, see readme for why
-# systemctl --user enable --now sxhkd.path
