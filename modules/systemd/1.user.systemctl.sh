a61de40fe88fe0eed1e23192f6b44ae79616690d
AlexAegis <alexaegis@gmail.com>
2020-04-09 03:43:13 +0200

    sx restart

--- modules/systemd/1.user.systemctl.sh
+++ modules/systemd/1.user.systemctl.sh
@@ -2,7 +2,6 @@
 
 systemctl enable --user --now user-service-watcher.service
 systemctl enable --user --now environment-watcher.service
-systemctl enable --user --now environment-watcher.target
 # Don't put --now here, can't enable without instance name.
 # And they will be only started by other services
 systemctl enable --user daemon-reload.service
