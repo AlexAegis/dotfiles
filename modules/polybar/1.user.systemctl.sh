a1e079ec4c7e4cc7cca23415d0f2e8b71bf3b50b
AlexAegis <alexaegis@gmail.com>
2020-04-13 02:41:29 +0200

    poly

--- modules/polybar/1.user.systemctl.sh
+++ modules/polybar/1.user.systemctl.sh
@@ -2,5 +2,7 @@
 
 systemctl enable --user --now polybar@main:1.service
 systemctl enable --user --now polybar@main:2.service
-systemctl enable --user --now polybar-watcher@main:1.service
-systemctl enable --user --now polybar-watcher@main:2.service
+systemctl enable --user polybar-watcher@main:1.service
+systemctl enable --user polybar-watcher@main:2.service
+systemctl enable --user polybar-environment-watcher@main:1.service
+systemctl enable --user polybar-environment-watcher@main:2.service
