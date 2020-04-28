55a60a717f9f55ee48e34cc2e11a0b3fe7336cc9
AlexAegis <alexaegis@gmail.com>
2020-04-28 11:47:25 +0200

    ssh

--- modules/ssh/init.user.sh
+++ modules/ssh/init.user.sh
@@ -3,4 +3,5 @@
 # If restored the `~/.ssh` folder, then not removing those
 # files that are present in the module and in the .ssh folder
 # would cause conflicts.
-rm "$HOME/.ssh/config"
+mkdir -p "$HOME/.ssh"
+[ -e "$HOME/.ssh/config" ] && rm "$HOME/.ssh/config"
