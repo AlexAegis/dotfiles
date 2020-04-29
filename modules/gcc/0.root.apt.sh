e61c48eccd0dfc3494f726e08aa6a3bdbdd40833
AlexAegis <alexaegis@gmail.com>
2020-04-29 09:13:23 +0200

    home gcc

--- modules/gcc/0.root.apt.sh
+++ modules/gcc/0.root.apt.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-apt install -y gcc llvm libclang-dev
+# TODO: Split this into multiple modules,
+# clang libtool-bin m4 autotools-dev automake gettext
+# were only added to build nvim
+
+apt install -y gcc llvm libclang-dev clang libtool-bin \
+	m4 autotools-dev automake gettext
