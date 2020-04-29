5d427d859a5987b0ef6b6ea6a3920a668773c79a
AlexAegis <alexaegis@gmail.com>
2020-04-29 12:34:55 +0200

    fnm updated vim colorscheme safety

--- modules/fnm/1.user.sh
+++ modules/fnm/1.user.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-# source them for the sake of installation
+# The reason why this is duplicated because 50-fnm-env.sh requires fnm to run
+# And upon installing this module, 50-fnm-env.sh in e.user.sh does nothing.
+# But once it get's here, fnm will be installed and it can!
+# But it's useful to have the env script here so other modules that need node
+# can rely on it being enabled by fnm
 # shellcheck disable=SC1090
-. "$XDG_CONFIG_HOME/environment.d/50-fnm.conf"
-. "$XDG_CONFIG_HOME/rc.d/fnm.sh"
+. "XDG_CONFIG_HOME.fnm/rc.d/50-fnm-env.sh"
 
 fnm install latest
-fnm install latest-v13.x
 fnm install latest-v12.x
 fnm install latest-v10.x
 fnm use latest
