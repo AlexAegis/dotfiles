4f17795fa41ced4d9f46f1ea545ffe362c8e5874
AlexAegis <alexaegis@gmail.com>
2020-05-02 18:46:00 +0200

    vim wsl

--- modules/git/2.user.wsl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/\
-libexec/git-core/git-credential-manager.exe"
--- modules/rust/1.user.sh
+++ modules/rust/1.user.sh
@@ -4,3 +4,6 @@ rustup install nightly
 rustup default stable
 
 rustup component add rls
+
+# Cargo extensions
+cargo install cargo-tree
