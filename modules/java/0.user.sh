2b6c6e65a16ea30433b2acba61b7234182c96847
AlexAegis <alexaegis@gmail.com>
2020-05-03 19:16:54 +0200

    jabba

--- modules/java/0.user.sh
+++ modules/java/0.user.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# shellcheck disable=SC1090
-. "$XDG_CONFIG_HOME/environment.d/50-jabba.conf"
-
 # instead of letting it modify startup scripts to source itself...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | \
 	/bin/bash -s -- --skip-rc && . "$JABBA_HOME/jabba.sh"
@@ -10,6 +7,6 @@ curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | \
 # ...I symlink the necessary files into my drop-in sourcables
 (
 	cd "$XDG_CONFIG_HOME/rc.d" || exit 1
-	ln -s "$JABBA_HOME/jabba.sh" jabba.sh
-	ln -s "$JABBA_HOME/jabba.fish" jabba.fish
+	ln -sf "$JABBA_HOME/jabba.sh" jabba.sh
+	ln -sf "$JABBA_HOME/jabba.fish" jabba.fish
 )
---
+++ modules/java/e.user.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+. "XDG_CONFIG_HOME.java/environment.d/50-jabba.conf"
+
