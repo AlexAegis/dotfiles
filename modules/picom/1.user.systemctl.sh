747086369ad4249d1038994db9f4cf53efa2b3be
AlexAegis <alexaegis@gmail.com>
2020-05-19 23:01:36 +0200

    changed picom forks

--- modules/picom/1.user.systemctl.sh
+++ modules/picom/1.user.systemctl.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-# systemctl enable --user --now picom
+# temporary, until it's not merged, use this fork
+
+cd ./resources/picom || exit 1
+
+pacman -Syu --needed --noconfirm meson uthash
+
+meson --buildtype=release . build
+
+ninja -C build
+
+ln -sf \
+	"$DOTFILES_HOME"/modules/picom/resources/compton/build/src/picom \
+	"$HOME"/.local/bin/picom
