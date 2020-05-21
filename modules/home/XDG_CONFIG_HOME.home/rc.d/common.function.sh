ec9988a40f91ab091a428d02c89517b932347d3a
AlexAegis <alexaegis@gmail.com>
2020-05-22 01:41:36 +0200

    alternative wallpaper selection

--- modules/home/XDG_CONFIG_HOME.home/rc.d/common.function.sh
+++ modules/home/XDG_CONFIG_HOME.home/rc.d/common.function.sh
@@ -15,3 +15,33 @@ matching_parents() {
 	# shellcheck disable=SC2016
 	grep -lm 1 -- "" "$@" | sed -r 's_^.*/([^/]*)/[^/]*$_\1_g'
 }
+
+to_absolute() {
+	# Coerces the argument as an absolute path
+	if [ "$1" ]; then
+		if [ "${1##/}" = "$1" ]; then
+			echo "$PWD/$1"
+		else
+			echo "$1"
+		fi
+	else
+		echo "$PWD"
+	fi
+}
+
+is_image() {
+	# pipable function, checks each file from stdin at pwd or the
+	# argument that its an image or not
+	while read -r input; do
+		abs_path="$(to_absolute "$1")/$input"
+      	if file -b "$abs_path" | grep image > /dev/null; then
+			echo "$abs_path"
+		fi
+  	done
+}
+
+last_modified_image() {
+	# returns the last modified images absolute path at the argument
+	target_dir="$(to_absolute "$1")"
+	/bin/ls -t1 "$target_dir" | is_image "$target_dir" | head -n 1
+}
--- modules/wal/XDG_CONFIG_HOME.wal/wal/wal.sh
+++ modules/wal/XDG_CONFIG_HOME.wal/wal/wal.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+# shellcheck disable=SC1090
+if [ -e "$XDG_CONFIG_HOME/rc.d/common.function.sh" ]; then
+	. "$XDG_CONFIG_HOME/rc.d/common.function.sh"
+fi
+
+if [ ! -e "${WALLPAPER}" ] || [ "${WALLPAPER_FORCE_LAST}" ]; then
+	# last_modified_image is defined in the home modules common.function.sh
+	WALLPAPER="$(last_modified_image "${WALLPAPERS_DIR}")"
+fi
+
 wal --backend haishoku --saturate 0.4 -i "${WALLPAPER}"
+
+# polybar doesn't really want to restart if set wal here, so I poke it
+# manually
+if [ -e "$XDG_CONFIG_HOME/polybar/config.ini" ]; then
+	touch "$XDG_CONFIG_HOME/polybar/config.ini"
+fi
