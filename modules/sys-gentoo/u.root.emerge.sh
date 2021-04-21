faa3a8af6df3426e27c9b828c6fa1cd6ab50ec06
AlexAegis <alexaegis@gmail.com>
2021-04-21 23:07:01 +0200

    feat(sys-gentoo): timelocked sync

--- modules/sys-gentoo/u.root.emerge.sh
+++ modules/sys-gentoo/u.root.emerge.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-emerge --sync
-emaint sync -A # Sync all repositories
+if timelock emerge_sync; then
+	emerge --sync
+	emaint sync -A # Sync all repositories
+	echo "Locking Sync for 24 hours using the emerge_sync lock"
+	timelock emerge_sync 'tomorrow'
+else
+	echo "The emerge_sync timelock is locked,\
+ it unlocks at $(timelock -p emerge_sync)"
+fi
 emerge --update --changed-use --deep @world
 emerge --depclean
 
+# shellcheck disable=SC1073 disable=SC1072 disable=SC1140 disable=SC1050
