19731b1e0255e71ffd2bb7e2d247d479888849d3
AlexAegis <alexaegis@gmail.com>
2021-06-06 19:17:37 +0200

    feat(minecraft): migrated to paru

--- modules/minecraft/0.user.paru.sh
+++ modules/minecraft/0.user.paru.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-yay -Syu --needed --noconfirm minecraft-launcher
+paru -Syu --needed --noconfirm minecraft-launcher
 
 # todo create user for the server and install it for them minecraft-server
