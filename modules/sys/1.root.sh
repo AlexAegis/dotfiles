434ccb6684fdc115dc9bec1dcd6cf6ad5140032d
AlexAegis <alexaegis@gmail.com>
2021-06-06 16:13:10 +0200

    fix(sys): initial folders

--- modules/sys/1.root.sh
+++ modules/sys/1.root.sh
@@ -2,5 +2,6 @@
 
 # enable sub-pixel RGB rendering globally
 
+mkdir -p /etc/fonts/conf.d
 cd /etc/fonts/conf.d || exit 1
 ln -sf ../conf.avail/10-sub-pixel-rgb.conf 10-sub-pixel-rgb.conf
--- modules/sys/init.user.sh
+++ modules/sys/init.user.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+mkdir -p "$XDG_CONFIG_HOME/environment.d"
 mkdir -p "$XDG_CONFIG_HOME/fontconfig/conf.d"
+mkdir -p "$XDG_CONFIG_HOME/gtk-3.0"
+mkdir -p "$XDG_CONFIG_HOME/sxhkd/sxhkdrc.d"
