c76c963447f8289457110d3269d19cea7fb4d246
AlexAegis <alexaegis@gmail.com>
2021-06-06 18:27:12 +0200

    chore(sys-arch): migrated to paru

---
+++ modules/sys-arch/1.user.paru.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Bug, see readme
+# paru -Syu --needed --noconfirm systemd-numlockontty
+
+paru -Syu --needed --noconfirm font-manager-git
--- modules/sys-arch/1.user.yay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Bug, see readme
-# yay -Syu --needed --noconfirm systemd-numlockontty
-
-yay -Syu --needed --noconfirm font-manager-git
