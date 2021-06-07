164af3af048ebde291bb21957574147cf43e7aef
AlexAegis <alexaegis@gmail.com>
2021-06-07 23:58:35 +0200

    fix(vim): fixed coc config location

--- modules/vim/1.user.sh
+++ modules/vim/1.user.sh
@@ -5,4 +5,4 @@
 curl -fLo "$XDG_CONFIG_HOME/vim/autoload/plug.vim" --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +PlugInstall +CocUpdateSync +qall
+vim +PlugInstall +PlugUpdate +CocUpdateSync +qall
