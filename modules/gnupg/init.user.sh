0ff9d0abef351ea7c6736b11b9f233aa238116a6
AlexAegis <alexaegis@gmail.com>
2021-06-08 08:43:14 +0200

    feat(gnupg): autobackup the one in home if already there

--- modules/gnupg/init.user.sh
+++ modules/gnupg/init.user.sh
@@ -3,3 +3,12 @@
 # Because of the backups
 mkdir -p "$XDG_CONFIG_HOME/gnupg"
 chmod 700 "$XDG_CONFIG_HOME/gnupg"
+
+if [ -d "$HOME/.gnupg" ]; then
+	if [ -d "$XDG_CACHE_HOME/gnupgbak" ]; then
+		rm -r "$XDG_CACHE_HOME/gnupgbak"
+	fi
+
+	mkdir -p "$XDG_CACHE_HOME/gnupgbak"
+	mv "$HOME/.gnupg" "$XDG_CACHE_HOME/gnupgbak"
+fi
