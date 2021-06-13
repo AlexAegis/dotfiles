0b60868a1016888bfe47d6c102b33586b4f93ac6
AlexAegis <alexaegis@gmail.com>
2021-06-13 17:22:56 +0200

    feat(sudo): mac

--- modules/sudo/1.root.$mac.sh
+++ modules/sudo/1.root.$mac.sh
@@ -2,8 +2,7 @@
 
 # Install the wheel sudoer file and making sure the 'wheel' group exists
 # USER will be expanded here so don't worry it being `root` once executed
-if [ ! "$mac" ]; then
-	su -c "./resources/install-sudoer ./resources/10-wheel && \
-		groupadd --force wheel && \
-		usermod -aG wheel $USER"
-fi
+# macs `su` doesn't default to root, but sudo is preinstalled
+./resources/install-sudoer ./resources/10-wheel
+
+dseditgroup -o edit -a "$USER" -t user wheel
