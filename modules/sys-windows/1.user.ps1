2859525736912038528c5030dfeafc3b1941f0c2
AlexAegis <alexaegis@gmail.com>
2021-06-13 21:25:54 +0200

    feat(sys-windows): added windows module

---
+++ modules/sys-windows/1.user.ps1
@@ -0,0 +1,4 @@
+#!/usr/bin/env powershell
+
+# use UTC instead of localtime
+reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_DWORD /f
