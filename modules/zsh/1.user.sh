7fc06c2c752d9f8c4fbec56bf9c523e86bad1876
AlexAegis <alexaegis@gmail.com>
2021-06-18 20:17:45 +0200

    fix(zsh): moved update to updatefile

--- modules/zsh/1.user.sh
+++ modules/zsh/1.user.sh
@@ -15,13 +15,12 @@ echo "Installing plugins"
 "$ZDOTDIR/bin/antibody" bundle <"$ZDOTDIR/.zsh-plugins.antibody" \
 	>"$ZDOTDIR/.zsh-plugins.antibody.sh"
 
-# TODO: call update and move update
-echo "Updating plugins"
-"$ZDOTDIR/bin/antibody" update
+# shellcheck disable=SC1091
+. "./u.user.sh"
 
 echo "Fixing permissions"
-
 chmod -R 700 "$XDG_CACHE_HOME/antibody"
 chmod -R 700 "$XDG_CONFIG_HOME/zsh"
+compaudit | xargs chmod g-w,o-w
 
 # TODO: Apply zsh-defer source to applicable plugins
--- modules/zsh/u.user.sh
+++ modules/zsh/u.user.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo "Updating plugins"
+
 # if it has problems regarding changes in the cache just delete it and reinstall
 # the module rm -rf ~/.cache/antibody/                                                                                                               ✔  23:05:50
-echo "Updating plugins"
 rm "$ZDOTDIR"/.zcompdump*
-antibody update
+"$ZDOTDIR/bin/antibody" update
