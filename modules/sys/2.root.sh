80e4a86d4a5cac3e7f656801cebb320e98d3026b
AlexAegis <alexaegis@gmail.com>
2021-06-18 20:53:26 +0200

    feat(sys): simplified bell disable

--- modules/sys/2.root.sh
+++ modules/sys/2.root.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-setting='set bell-style none'
-file='/etc/inputrc'
-
-sed -i "s/^# *\($setting\)/\1/g" "$file" # Try uncommenting first
-
-if ! grep -q -x "$setting" "$file"; then # If still not present, append
-	echo "$setting" >> "$file"
-fi
+appendabsent 'set bell-style none' '/etc/inputrc'
