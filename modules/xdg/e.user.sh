1cb243d2f94532fc2ddabb8d98aeee0365ada222
AlexAegis <alexaegis@gmail.com>
2021-06-18 16:57:30 +0200

    feat(home): loadenv use xdg directly to start

--- modules/xdg/e.user.sh
+++ modules/xdg/e.user.sh
@@ -3,7 +3,9 @@
 # shellcheck disable=SC1091
 . "./XDG_CONFIG_HOME.xdg/environment.d/10-xdg.conf"
 
-if [ "$mac" ]; then
+# Checking by itself, instead of utilizing the $mac variable set by pont
+# so that the xdg/e.user.sh is usable on it's own
+if [ "$(uname -s)" = "Darwin" ]; then
 	# shellcheck disable=SC1091
 	. "./XDG_CONFIG_HOME.\$mac.xdg/environment.d/11-xdg-mac.conf"
 fi
