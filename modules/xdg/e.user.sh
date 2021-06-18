45cfbe5141454a97f143f279aca370c013cb6bc0
AlexAegis <alexaegis@gmail.com>
2021-06-18 12:20:19 +0200

    fix(xdg): mac pre-environment

--- modules/xdg/e.user.sh
+++ modules/xdg/e.user.sh
@@ -2,3 +2,8 @@
 
 # shellcheck disable=SC1091
 . "./XDG_CONFIG_HOME.xdg/environment.d/10-xdg.conf"
+
+if [ "$mac" ]; then
+	# shellcheck disable=SC1091
+	. "./XDG_CONFIG_HOME.\$mac.xdg/environment.d/11-xdg-mac.conf"
+fi
