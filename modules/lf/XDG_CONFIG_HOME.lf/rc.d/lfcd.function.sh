342d30148cfd382e3d52ca6ab6e004aa16267c1c
AlexAegis <alexaegis@gmail.com>
2021-06-19 00:02:05 +0200

    chore(lf): update lfcd

--- modules/lf/XDG_CONFIG_HOME.lf/rc.d/lfcd.function.sh
+++ modules/lf/XDG_CONFIG_HOME.lf/rc.d/lfcd.function.sh
@@ -18,13 +18,13 @@
 
 lfcd() {
 	tmp="$(mktemp)"
-	/usr/bin/lf -last-dir-path="$tmp" "$@"
+	lf -last-dir-path="$tmp" "$@"
 	if [ -f "$tmp" ]; then
 		dir="$(cat "$tmp")"
 		rm -f "$tmp"
 		if [ -d "$dir" ]; then
 			if [ "$dir" != "$(pwd)" ]; then
-				cd "$dir" || exit 1
+				cd "$dir"
 			fi
 		fi
 	fi
