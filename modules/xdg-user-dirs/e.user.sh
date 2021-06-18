b0b62ab8cdd5345685d30c77f0d2d61db529966f
AlexAegis <alexaegis@gmail.com>
2021-06-18 12:31:34 +0200

    feat(xdg-user-dirs): mac compatibility

--- modules/xdg-user-dirs/e.user.sh
+++ modules/xdg-user-dirs/e.user.sh
@@ -2,3 +2,8 @@
 
 # shellcheck disable=SC1091
 . "./XDG_CONFIG_HOME.xdg-user-dirs/environment.d/11-xdg-user-dirs.conf"
+
+if [ "$mac" ]; then
+	# shellcheck disable=SC1091
+	. "./XDG_CONFIG_HOME.\$mac.xdg-user-dirs/environment.d/12-xdg-user-dirs-mac.conf"
+fi
--- modules/xdg-user-dirs/i.user.sh
+++ modules/xdg-user-dirs/i.user.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# making sure it's the syslink if `xdg-user-dirs-update` overwrites it
+# making sure it's the symlink if `xdg-user-dirs-update` overwrites it
+# ? Remember: this step happens before symlinking!
 [ -e "$XDG_CONFIG_HOME/user-dirs.dirs" ] \
 	&& rm "$XDG_CONFIG_HOME/user-dirs.dirs"
 
@@ -13,12 +14,3 @@ mkdir -p "$XDG_DESKTOP_DIR" \
 		"$XDG_TEMPLATES_DIR" \
 		"$XDG_VIDEOS_DIR"
 
-# rm -r ~/Videos ~/Desktop ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
-# rm -r "$XDG_DOCUMENTS_DIR" \
-# 		"$XDG_DOWNLOAD_DIR" \
-# 		"$XDG_MUSIC_DIR" \
-# 		"$XDG_PICTURES_DIR" \
-# 		"$XDG_PUBLICSHARE_DIR" \
-# 		"$XDG_TEMPLATES_DIR" \
-# 		"$XDG_VIDEOS_DIR"
-#
