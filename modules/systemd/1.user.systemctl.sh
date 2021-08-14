629f986b70e00e86730ea07afbda16cc5a651eae
AlexAegis <alexaegis@gmail.com>
2021-08-14 12:45:38 +0200

    fix(systemd): removed unnecessary enable commands

--- modules/systemd/1.user.systemctl.sh
+++ modules/systemd/1.user.systemctl.sh
@@ -5,8 +5,3 @@ systemctl enable --user --now environment-watcher.service
 # Don't put --now here, can't enable without instance name.
 # And they will be only started by other services
 systemctl enable --user daemon-reload.service
-systemctl enable --user notify-low@.service
-systemctl enable --user notify-normal@.service
-systemctl enable --user notify-critical@.service
-systemctl enable --user notify-failure@.service
-systemctl enable --user notify-restart@.service
