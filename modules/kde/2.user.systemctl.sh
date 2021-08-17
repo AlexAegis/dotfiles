4e367526086e126ff5e8af518f9091574fb25fb2
AlexAegis <alexaegis@gmail.com>
2021-08-17 22:59:14 +0200

    feat(kde): reenabled systemd plasma launch and removed bad override config

--- modules/kde/2.user.systemctl.sh
+++ modules/kde/2.user.systemctl.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # Enable systemd startup (since plasma 5.20)
-# TODO: might or might not be a good idea
-# kwriteconfig5 --file startkderc --group General --key systemdBoot true
+kwriteconfig5 --file startkderc --group General --key systemdBoot true
