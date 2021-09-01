287c81b274c5ceff5c9bfb342614a297351d4436
AlexAegis <alexaegis@gmail.com>
2021-09-01 23:06:10 +0200

    fix(powerline): fixed font discovery

--- modules/powerline/1.root.sh
+++ modules/powerline/1.root.sh
@@ -3,9 +3,8 @@
 # Copy vconsole fonts
 if [ -d "$VCONSOLE_FONT_DIR" ]; then
 	find "$POWERLINE_DIR" \
-		-print0 \
 		-regex '.*/PCF/.*\.pcfu?.gz' \
-		-o -regex '.*/PSF/.*\.psfu?.gz' | while IFS=$(printf '\0') read -r file
+		-o -regex '.*/PSF/.*\.psfu?.gz' | while read -r file
 	do
 		cp "$file" "$VCONSOLE_FONT_DIR"
 	done
