3fb55f9c6ee9ee5a1d8437055932dcf8326ac717
AlexAegis <alexaegis@gmail.com>
2021-09-02 21:49:12 +0200

    feat(nginx): initial config

---
+++ modules/nginx/1.root.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Create service user
+useradd -M -U http
+
+# shellcheck disable=SC1091
+. "./u.root.sh"
---
+++ modules/nginx/u.root.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Copy resources
+cp -r ./resources/etc/nginx/* /etc/nginx/
