d900ea780b77be80a62370f6750810116b2d22bd
AlexAegis <alexaegis@gmail.com>
2021-09-02 20:37:57 +0200

    fix(vim): create undodir and other locations

--- modules/vim/i.user.sh
+++ modules/vim/i.user.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+# To prevent symlinking folders
 mkdir -p \
+	"$XDG_CONFIG_HOME/vim/backup" \
 	"$XDG_CONFIG_HOME/vim/bundle" \
 	"$XDG_CONFIG_HOME/vim/autoload" \
 	"$XDG_CONFIG_HOME/vim/plugin" \
 	"$XDG_CONFIG_HOME/vim/colors" \
 	"$XDG_CONFIG_HOME/coc/extensions" \
 	"$XDG_CONFIG_HOME/nvim"
+
+# Vim will not create these folders for you
+# https://vimhelp.org/options.txt.html#%27backupdir%27
+mkdir -p \
+	"$XDG_CACHE_HOME/vim/swap" \
+	"$XDG_CACHE_HOME/tmp" \
+	"$XDG_CONFIG_HOME/vim/backup" \
+	"$XDG_CONFIG_HOME/vim/undo" \
+	"$XDG_CONFIG_HOME/vim/after"
