e9fc416969781b423b58570dad03ceaf019266da
AlexAegis <alexaegis@gmail.com>
2021-09-02 23:24:20 +0200

    feat(nginx): force create default nginx folders

---
+++ modules/nginx/e.user.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. "./XDG_CONFIG_HOME.nginx/environment.d/10-nginx.conf"
--- modules/nginx/i.root.sh
+++ modules/nginx/i.root.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-mkdir -p /etc/nginx/conf.d/root
+mkdir -p \
+	/etc/nginx/conf.d/ \
+	/etc/nginx/modules-available/ \
+	/etc/nginx/modules-enabled/ \
+	/etc/nginx/sites-available/ \
+	/etc/nginx/sites-enabled/ \
+	/etc/nginx/snippets/ \
