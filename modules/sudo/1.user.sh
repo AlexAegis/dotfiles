e6b7649baf5fcccd972115b42fad6df8d8b5adc1
AlexAegis <alexaegis@gmail.com>
2021-11-14 22:21:32 +0100

    fix(sudo): command order fixed

--- modules/sudo/1.user.sh
+++ modules/sudo/1.user.sh
@@ -2,9 +2,9 @@
 
 # Install the wheel sudoer file and making sure the 'wheel' group exists
 # USER will be expanded here so don't worry it being `root` once executed
-command="./resources/install-sudoer ./resources/10-wheel && \
-		groupadd --force wheel && \
-		usermod -aG wheel $USER"
+command="groupadd --force wheel && \
+		 usermod -aG wheel $USER && \
+		 ./resources/install-sudoer ./resources/10-wheel"
 
 if  [ ! "$mac" ]; then
 	if command -v sudo >/dev/null; then
