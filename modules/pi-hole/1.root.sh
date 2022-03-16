694a1877dcef3a7f691efe1aaea04e17781a6a15
AlexAegis <alexaegis@gmail.com>
2022-03-17 00:14:28 +0100

    lint(pi-hole): added quotes to prevent splitting

--- modules/pi-hole/1.root.sh
+++ modules/pi-hole/1.root.sh
@@ -5,7 +5,7 @@
 
 pihole -a -p # Ask for a password for the admin panel
 pihole -a -c # Use Celsius
-pihole -a -e $(git config user.email) # Set contact mail
+pihole -a -e "$(git config user.email)" # Set contact mail
 
 # Install pihole-updatelist
 wget -O - https://raw.githubusercontent.com/jacklul/pihole-updatelists/master/install.sh | bash
