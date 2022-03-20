19f6c5c16180c917234f070e2e982c7bf3e98195
AlexAegis <alexaegis@gmail.com>
2022-03-20 01:41:56 +0100

    fix(netdata): updated update script

--- modules/netdata/u.root.sh
+++ modules/netdata/u.root.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Enable site
-echo $NGINX_SITES_ENABLED
-cp ./resources/site-netdata.conf "$NGINX_SITES_ENABLED/"
+cp ./resources/nginx.netdata.conf "$NGINX_SITES_ENABLED/"
 reload_service nginx
