f11e9c4fd434b19faacba22eb1788ae0e84d7615
AlexAegis <alexaegis@gmail.com>
2022-03-20 01:39:57 +0100

    fix(tty-screensaver): return to tty1 after screensaver stopped

--- modules/tty-screensaver/1.root.systemctl.sh
+++ modules/tty-screensaver/1.root.systemctl.sh
@@ -4,5 +4,10 @@ mkdir -p /etc/systemd/system/getty@tty1.service.d
 cp ./resources/getty@tty1.service.d/override-bottom.conf \
 	/etc/systemd/system/getty@tty1.service.d/override.conf
 
+# Lightdm return to tty1
+mkdir -p /etc/systemd/system/lightdm.service.d/
+cp ./resources/override-return.conf \
+	/etc/systemd/system/lightdm.service.d/override.conf
+
 systemctl daemon-reload
 systemctl restart getty@tty1
