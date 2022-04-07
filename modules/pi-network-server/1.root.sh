374aa73ba8d5b44cb4addb085aa102624f2dcb1e
AlexAegis <alexaegis@gmail.com>
2022-04-08 01:08:20 +0200

    fix(pi-network-server): recursive chown

--- modules/pi-network-server/1.root.sh
+++ modules/pi-network-server/1.root.sh
@@ -3,6 +3,6 @@
 groupadd --force media
 # Create media directory
 mkdir -p /var/media
-chown root:media /var/media
+chown -R root:media /var/media
 # Add your user to the group for access
 usermod -aG media "${SUDO_USER:-USER}"
