af27e979410dbe99a014e984b91503b5c6e8f69a
AlexAegis <alexaegis@gmail.com>
2022-04-08 20:00:36 +0200

    feat(samba): use crudini

--- modules/samba/1.root.sh
+++ modules/samba/1.root.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-# Samba only has a single configuration file so I'm appending to it.
-# This, without a proper ini editor wont allow for easy edits by changing
-# this file
-# TODO: Refactor with ini editor
+# Samba only has a single configuration file
 mkdir -p /etc/samba
 touch /etc/samba/smb.conf
-if ! grep -qEi "\[global\]" /etc/samba/smb.conf >/dev/null; then
-cat << EOF >> /etc/samba/smb.conf
+
+ini-unindent /etc/samba/smb.conf
+crudini --merge /etc/samba/smb.conf << EOF
 [global]
 security = user
 log file = /var/log/samba/%m.log
 logging = systemd
 EOF
-fi
-
+ini-indent /etc/samba/smb.conf
