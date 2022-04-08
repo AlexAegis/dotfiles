628e21cabc3f21ab5536da8bc757c9e50c9bb78a
AlexAegis <alexaegis@gmail.com>
2022-04-08 20:36:57 +0200

    feat(samba): update script with service reload

--- modules/samba/2.root.systemctl.sh
+++ modules/samba/2.root.systemctl.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-systemctl enable --now smb.service
-systemctl enable --now nmb.service
-systemctl enable --now wsdd.service
+# shellcheck source=/dev/null
+. './u.user.sh'
---
+++ modules/samba/u.user.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Set up a samba password for the user if not already
+
+
+reload_service smb.service
+reload_service nmb.service
+reload_service wsdd.service
