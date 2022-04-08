5b082b2f8aeb16a273cf94e9ce53570053748df3
AlexAegis <alexaegis@gmail.com>
2022-04-08 20:24:44 +0200

    fix(samba): stop asking for password on subsequent runs

--- modules/samba/3.root.sh
+++ modules/samba/3.root.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-# Set up a samba password for the user
-smbpasswd -a "${SUDO_USER:-$USER}"
+# Set up a samba password for the user if not already
+
+existing_user=$(pdbedit -L -v | awk "/Unix username:.*${SUDO_USER:-$USER}/ { print \$3 }")
+
+if [ -z "$existing_user" ]; then
+	smbpasswd -a "${SUDO_USER:-$USER}"
+else
+	echo "Samba user $existing_user already exists!
+To change password use smbpasswd -a <user>"
+fi
