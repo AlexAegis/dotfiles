aa3c5e349fe21227b7d03e52daa5e368ee5482ba
AlexAegis <alexaegis@gmail.com>
2022-04-10 21:30:33 +0200

    feat(sys-arch): added uboot-tools to 64bit arm arch install

--- modules/sys-arch/0.root.pacman.sh
+++ modules/sys-arch/0.root.pacman.sh
@@ -5,3 +5,8 @@ pacman -Syu --needed --noconfirm arch-install-scripts
 
 # ? Might move into it's own module?
 pacman -Syu --needed --noconfirm accountsservice
+
+# 64-bit ARM Arch uses uboot, it's useful to have it's tools installed
+if [ "$(uname -m)" = 'aarch64' ]; then
+	pacman -Syu --needed --noconfirm uboot-tools
+fi
