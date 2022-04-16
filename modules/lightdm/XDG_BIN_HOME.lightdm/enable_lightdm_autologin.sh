0a5acab2963f780c4924fe4c1e6fcb8cf1830322
AlexAegis <alexaegis@gmail.com>
2022-04-17 00:20:43 +0200

    fix(lightdm): normalize username

--- modules/lightdm/XDG_BIN_HOME.lightdm/enable_lightdm_autologin.sh
+++ modules/lightdm/XDG_BIN_HOME.lightdm/enable_lightdm_autologin.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+username="${SUDO_USER:-$USER}"
+
 if ! getent group "autologin" 1>/dev/null; then
 	sudo groupadd -r autologin
 fi
-sudo gpasswd -a "$USER" autologin
+sudo gpasswd -a "$username" autologin
 
-
-sed -i -e "s/^.*autologin-user=.*/autologin-user=$USER/" \
+sudo sed -i -e "s/^.*autologin-user=.*/autologin-user=$username/" \
 	/etc/lightdm/lightdm.conf
