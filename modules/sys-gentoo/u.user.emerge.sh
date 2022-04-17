5d04dc66aa82a2c8794ca10ee9161bb3b64fdac7
AlexAegis <alexaegis@gmail.com>
2022-04-17 02:39:30 +0200

    fix(sys-gentoo): dont run timelock as root

--- modules/sys-gentoo/u.user.emerge.sh
+++ modules/sys-gentoo/u.user.emerge.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+# Timelock better not run as root
+
 if timelock emerge_sync; then
-	emerge --sync
-	emaint sync -A # Sync all repositories
+	sudo emerge --sync
+	sudo emaint sync -A # Sync all repositories
 	echo "Locking Sync for 24 hours using the emerge_sync lock"
 	timelock emerge_sync 'tomorrow'
 else
 	echo "The emerge_sync timelock is locked,\
  it unlocks at $(timelock -p emerge_sync)"
 fi
-emerge --update --changed-use --deep @world
-emerge --depclean
+sudo emerge --update --changed-use --deep @world
+sudo emerge --depclean
