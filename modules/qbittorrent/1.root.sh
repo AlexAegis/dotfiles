e568946b9efa10b119af405f05666669d072a3a1
AlexAegis <alexaegis@gmail.com>
2022-04-17 21:31:05 +0200

    fix(qbittorrent): add torrent user to media group

--- modules/qbittorrent/1.root.sh
+++ modules/qbittorrent/1.root.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # the user of qbittorrent needs a home folder, it will cache there
-try_create_service_user.sh torrent media
+try_create_service_user.sh torrent
+try_create_service_user.sh media
+usermod -aG media torrent
