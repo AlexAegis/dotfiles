d2b0d422196caca6e57411f5b9f82ec1f0521236
AlexAegis <alexaegis@gmail.com>
2022-04-17 21:42:30 +0200

    fix(emby): added the torrent group to the emby user

--- modules/emby/1.root.sh
+++ modules/emby/1.root.sh
@@ -3,6 +3,8 @@
 # Make sure the emby user and group exists
 try_create_service_user.sh emby
 try_create_service_user.sh media
+try_create_service_user.sh torrent
 usermod -aG media emby
+usermod -aG torrent emby
 # Link emby to /var/media
 ./XDG_BIN_HOME.emby/link_emby_database.sh /var/media/emby
