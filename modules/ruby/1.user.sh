da6519b934b7308926e0aee5b7e418e01ae95982
AlexAegis <alexaegis@gmail.com>
2022-11-20 02:54:47 +0100

    fix(ruby): change installation

--- modules/ruby/1.user.sh
+++ modules/ruby/1.user.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
-if [ -d "$RBENV_ROOT/.git" ]; then
-	(
-		cd "$RBENV_ROOT" || exit 1
-		git pull
-	)
-else
-	git clone https://github.com/rbenv/rbenv "$RBENV_ROOT"
-fi
+# if [ -d "$RBENV_ROOT/.git" ]; then
+# 	(
+# 		cd "$RBENV_ROOT" || exit 1
+# 		git pull
+# 	)
+# else
+# 	git clone https://github.com/rbenv/rbenv "$RBENV_ROOT"
+# fi
+#
+#
+#
+# # Install plugins
+#
+# if [ -d "$RBENV_ROOT/plugins/ruby-build/.git" ]; then
+# 	(
+# 		cd "$RBENV_ROOT/plugins/ruby-build" || exit 1
+# 		git pull
+# 	)
+# else
+# 	git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
+# fi
 
+## OR
 
-
-# Install plugins
-
-if [ -d "$RBENV_ROOT/plugins/ruby-build/.git" ]; then
-	(
-		cd "$RBENV_ROOT/plugins/ruby-build" || exit 1
-		git pull
-	)
-else
-	git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
-fi
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
