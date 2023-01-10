d2b97268335a01ad294737f4acc1b21a7e827860
AlexAegis <alexaegis@gmail.com>
2023-01-10 20:24:28 +0100

    chore(emby): updated to latest beta

--- modules/emby/0.root.pacman.sh
+++ modules/emby/0.root.pacman.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-emby_version="4.7.6.0"
+emby_version="4.8.0.21"
 emby_filename="emby-server-deb_${emby_version}_arm64.deb"
 emby_url="https://github.com/MediaBrowser/Emby.Releases/releases/download/${emby_version}/${emby_filename}"
 cache_dir="$XDG_CACHE_HOME/emby"
 cached_emby_installer="$cache_dir/$emby_filename"
+
+
+# if it fails you might want to delete the entire cache_dir
+# rm -rf "$cache_dir"
 
 distribution=$(grep "^NAME" /etc/os-release 2>/dev/null | grep -oh "=.*" | \
 	tr -d '="')
@@ -12,6 +16,7 @@ distribution=$(grep "^NAME" /etc/os-release 2>/dev/null | grep -oh "=.*" | \
 if [ "$distribution" = 'Arch Linux ARM' ]; then
 	if [ ! -f "$cached_emby_installer" ]; then
 		mkdir -p "$cache_dir"
+
 		wget "$emby_url" -O "$cached_emby_installer"
 
 		paru -Syu --needed --noconfirm debtap
