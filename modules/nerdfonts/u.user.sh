0a5118dee1304844ce418b863725526d4bf69cd9
AlexAegis <alexaegis@gmail.com>
2023-02-25 13:38:41 +0100

    fix(nerdfonts): dont update like this on arch

--- modules/nerdfonts/u.user.sh
+++ modules/nerdfonts/u.user.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ -d "$NERDFONTS_REPOSITORY/.git" ]; then
-	(
-		cd "$NERDFONTS_REPOSITORY" || exit 1
-		hash_before=$(git rev-parse HEAD)
-		git pull
-		hash_after=$(git rev-parse HEAD)
-		if [ "$hash_before" != "$hash_after" ]; then
-			"./install.sh"
-		fi
-	)
+if ! command -v pacman; then
+	if [ -d "$NERDFONTS_REPOSITORY/.git" ]; then
+		(
+			cd "$NERDFONTS_REPOSITORY" || exit 1
+			hash_before=$(git rev-parse HEAD)
+			git pull
+			hash_after=$(git rev-parse HEAD)
+			if [ "$hash_before" != "$hash_after" ]; then
+				"./install.sh"
+			fi
+		)
+	fi
 fi
