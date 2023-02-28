e19c808bc434e0943dc77df057acf831d1bd1d50
AlexAegis <alexaegis@gmail.com>
2023-02-28 23:27:39 +0100

    fix(wine): sound fix

--- modules/wine/0.root.pacman.sh
+++ modules/wine/0.root.pacman.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 pacman -Syu --needed --noconfirm wine winetricks
+
+# Sound won't work without these on Arch
+pacman -Syu --needed --noconfirm lib32-alsa-plugins lib32-libpulse lib32-openal
