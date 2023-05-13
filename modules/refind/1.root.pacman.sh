2e61e5c2030b5f3a10b107e2d6097e7dd8129427
AlexAegis <alexaegis@gmail.com>
2023-05-13 17:32:31 +0200

    feat(refind): updated theme

--- modules/refind/1.root.pacman.sh
+++ modules/refind/1.root.pacman.sh
@@ -3,7 +3,7 @@
 mkdir -p /etc/pacman.d/hooks/
 
 # ? This hook runs upgrades refind automatically if refind is installed
-install -Dm 755 "./resources/20-refind-upgrade-local.hook" \
+install -Dm 755 "./resources/hooks/20-refind-upgrade-local.hook" \
 	"/etc/pacman.d/hooks/20-refind-upgrade-local.hook"
-install -Dm 755 "./resources/21-refind-upgrade-mounted.hook" \
+install -Dm 755 "./resources/hooks/21-refind-upgrade-mounted.hook" \
 	"/etc/pacman.d/hooks/21-refind-upgrade-mounted.hook"
--- modules/refind/2.root.sh
+++ modules/refind/2.root.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-refind_apply_config
+if [ -z "$REFIND_ROOT" ]; then
+	if [ -d /mnt/refind/EFI/refind ]; then
+		REFIND_ROOT="/mnt/refind"
+	elif [ -d /boot/EFI/refind ]; then
+		REFIND_ROOT="/boot"
+	fi
+fi
+
+if [ -n "$REFIND_ROOT" ]; then
+	echo "Using Refind root at $REFIND_ROOT"
+	# update refind
+	refind-install --yes --root "$REFIND_ROOT"
+	# add custom config file
+	echo Copying resources to "$REFIND_ROOT"
+	cp -r resources/boot/* "$REFIND_ROOT"
+else
+	echo "No refind installation detected, mount it under /boot/EFI or " \
+		"/mnt/refind/boot/EFI"
+fi
+
