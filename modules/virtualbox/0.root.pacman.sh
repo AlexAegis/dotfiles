3a472669828a116d219b83cf5d623e8be0a4a0b1
AlexAegis <alexaegis@gmail.com>
2023-06-25 17:18:02 +0200

    feat: add guest-iso to virtualbox

--- modules/virtualbox/0.root.pacman.sh
+++ modules/virtualbox/0.root.pacman.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-pacman -Syu --needed --noconfirm virtualbox virtualbox-host-modules-arch
+pacman -Syu --needed --noconfirm \
+    virtualbox \
+    virtualbox-host-modules-arch \
+    virtualbox-guest-iso
 # For any other kernel than `linux`, like `linux-lts`, use virtualbox-host-dkms
