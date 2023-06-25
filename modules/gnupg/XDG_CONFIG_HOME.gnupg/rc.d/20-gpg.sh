7383da4a4c4e772e4cd6855ae68569129a102d68
AlexAegis <alexaegis@gmail.com>
2023-06-25 21:57:43 +0200

    fix(gnupg): connect agent output null

--- modules/gnupg/XDG_CONFIG_HOME.gnupg/rc.d/20-gpg.sh
+++ modules/gnupg/XDG_CONFIG_HOME.gnupg/rc.d/20-gpg.sh
@@ -3,7 +3,7 @@
 GPG_TTY=$(tty)
 export GPG_TTY
 
-gpg-connect-agent updatestartuptty /bye >/dev/null
+gpg-connect-agent updatestartuptty /bye > /dev/null 2> /dev/null
 
 SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 export SSH_AUTH_SOCK
