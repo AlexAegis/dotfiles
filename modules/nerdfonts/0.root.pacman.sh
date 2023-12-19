8158e2818a02d00226c024de075104fe23b135ad
AlexAegis <alexaegis@gmail.com>
2023-12-19 20:07:42 +0100

    feat: migrated from the removed nerd-fonts-meta aur package to individual nerd font packages

---
+++ modules/nerdfonts/0.root.pacman.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+pacman -Syu --needed --noconfirm \
+	otf-aurulent-nerd \
+	otf-codenewroman-nerd \
+	otf-comicshanns-nerd \
+	otf-commit-mono-nerd \
+	otf-droid-nerd \
+	otf-firamono-nerd \
+	otf-geist-mono-nerd \
+	otf-hasklig-nerd \
+	otf-hermit-nerd \
+	otf-monaspace-nerd \
+	otf-opendyslexic-nerd \
+	otf-overpass-nerd		 \
+	ttf-0xproto-nerd \
+	ttf-3270-nerd \
+	ttf-agave-nerd \
+	ttf-anonymouspro-nerd \
+	ttf-arimo-nerd \
+	ttf-bigblueterminal-nerd \
+	ttf-bitstream-vera-mono-nerd \
+	ttf-cascadia-code-nerd \
+	ttf-cascadia-mono-nerd \
+	ttf-cousine-nerd \
+	ttf-d2coding-nerd \
+	ttf-daddytime-mono-nerd \
+	ttf-dejavu-nerd \
+	ttf-envycoder-nerd \
+	ttf-fantasque-nerd \
+	ttf-firacode-nerd \
+	ttf-go-nerd \
+	ttf-hack-nerd \
+	ttf-heavydata-nerd \
+	ttf-iawriter-nerd \
+	ttf-ibmplex-mono-nerd \
+	ttf-inconsolata-go-nerd \
+	ttf-inconsolata-lgc-nerd \
+	ttf-inconsolata-nerd \
+	ttf-intone-nerd \
+	ttf-iosevka-nerd \
+	ttf-iosevkaterm-nerd \
+	ttf-jetbrains-mono-nerd \
+	ttf-lekton-nerd \
+	ttf-liberation-mono-nerd \
+	ttf-lilex-nerd \
+	ttf-meslo-nerd \
+	ttf-monofur-nerd \
+	ttf-monoid-nerd \
+	ttf-mononoki-nerd \
+	ttf-mplus-nerd \
+	ttf-nerd-fonts-symbols \
+	ttf-nerd-fonts-symbols-common \
+	ttf-nerd-fonts-symbols-mono \
+	ttf-noto-nerd \
+	ttf-profont-nerd \
+	ttf-proggyclean-nerd \
+	ttf-roboto-mono-nerd \
+	ttf-sharetech-mono-nerd \
+	ttf-sourcecodepro-nerd \
+	ttf-space-mono-nerd \
+	ttf-terminus-nerd \
+	ttf-tinos-nerd \
+	ttf-ubuntu-mono-nerd \
+	ttf-ubuntu-nerd \
+	ttf-victor-mono-nerd
+
+# ttf-martian-mono-nerd timed out
