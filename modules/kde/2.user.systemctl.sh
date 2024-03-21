372631a845e6f9e3eba9d54297a40463990e71b1
Sandor <alexaegis@pm.me>
2024-03-21 21:39:03 +0100

    feat: use kwriteconfig6 instead of 5

--- modules/kde/2.user.systemctl.sh
+++ modules/kde/2.user.systemctl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Enable systemd startup (since plasma 5.20)
-kwriteconfig5 --file startkderc --group General --key systemdBoot true
+kwriteconfig6 --file startkderc --group General --key systemdBoot true
