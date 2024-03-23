2bbd75b37f7d42fdb9af92829500d7cf4ca79203
AlexAegis <alexaegis@pm.me>
2024-03-23 14:52:37 +0100

    chore: add kcalc to preinstalled kde programs

--- modules/kde/0.root.pacman.sh
+++ modules/kde/0.root.pacman.sh
@@ -14,5 +14,5 @@ pacman -Syu --needed --noconfirm ksshaskpass
 # https://store.kde.org/ integration
 pacman -Syu --needed --noconfirm qt6-base qt6-svg qt6-declarative
 
-# Filebrowser, archive manager, disk usage utility
-pacman -Syu --needed --noconfirm dolphin ark filelight
+# Filebrowser, archive manager, disk usage utility, calculator
+pacman -Syu --needed --noconfirm dolphin ark filelight kcalc
