d2bbdd9c2948f6a5ba71aa799fdcf08a22875a72
AlexAegis <alexaegis@pm.me>
2024-05-01 23:59:11 +0200

    feat(kde): added kcolorchooser

--- modules/kde/0.root.pacman.sh
+++ modules/kde/0.root.pacman.sh
@@ -14,5 +14,5 @@ pacman -Syu --needed --noconfirm ksshaskpass
 # https://store.kde.org/ integration
 pacman -Syu --needed --noconfirm qt6-base qt6-svg qt6-declarative
 
-# Filebrowser, archive manager, disk usage utility, calculator
-pacman -Syu --needed --noconfirm dolphin ark filelight kcalc gwenview
+# Filebrowser, archive manager, disk usage utility, calculator etc
+pacman -Syu --needed --noconfirm dolphin ark filelight kcalc gwenview kcolorchooser
