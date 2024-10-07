6969efbf4fd0c042ad712bea18e36aab62f2be81
AlexAegis <alexaegis@pm.me>
2024-10-07 16:54:53 +0200

    fix(msfonts): remove old tarballs before makepkg

--- modules/msfonts/0.user.sh
+++ modules/msfonts/0.user.sh
@@ -2,7 +2,9 @@
 
 # aura -Ayu --noconfirm ttf-ms-win11-auto
 
-git_repo_url="https://aur.archlinux.org/ttf-ms-win11-auto.git"
+package_name="ttf-ms-win11-auto"
+
+git_repo_url="https://aur.archlinux.org/${package_name}.git"
 install_dir="$XDG_CACHE_HOME/ttf-ms-win11-auto"
 
 if ! [ -d "$install_dir/.git" ]; then
@@ -18,8 +20,9 @@ fi
 
 (
 	cd "$install_dir" || exit 1
+	rm "${package_name}"*.pkg.tar.zst
 	makepkg -si
-	sudo pacman -U --noconfirm ttf-ms-win11-auto*.pkg.tar.zst
+	sudo pacman -U --noconfirm "${package_name}"*.pkg.tar.zst
 )
 
 
