b3778eb96721decee095f5d572ef530f5fb56954
AlexAegis <alexaegis@pm.me>
2024-12-13 13:46:04 +0100

    chore(kde): disabled crudini step as crudini doesnt work because of python

--- modules/kde/1.user.sh
+++ modules/kde/1.user.sh
@@ -3,7 +3,7 @@
 # On KDE let it ask the pass for git
 git config --global core.askpass /usr/bin/ksshaskpass
 
-crudini --merge "$XDG_CONFIG_HOME/kwinrc" << EOF
-[Compositor]
-MaxFPS=144
-EOF
+# crudini --merge "$XDG_CONFIG_HOME/kwinrc" << EOF
+# [Compositor]
+# MaxFPS=144
+# EOF
