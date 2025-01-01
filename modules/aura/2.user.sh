5aeaa08976421b83d48e51f7c88211c5543bfac0
AlexAegis <alexaegis@pm.me>
2025-01-01 21:37:09 +0100

    fix(aura): create cache folders

--- modules/aura/2.user.sh
+++ modules/aura/2.user.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+mkdir -p "$XDG_CACHE_HOME"/aura/builds
+mkdir -p "$XDG_CACHE_HOME"/aura/cache
+mkdir -p "$XDG_CACHE_HOME"/aura/hashes
+mkdir -p "$XDG_CACHE_HOME"/aura/packages
+
 aura check
