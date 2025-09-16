f1933e09311111fea94915d3dc76dad260d27725
AlexAegis <alexaegis@pm.me>
2025-09-16 20:29:29 +0200

    fix(gnupg): do not override the ssh socket with gnupgs own

--- modules/gnupg/XDG_CONFIG_HOME.gnupg/rc.d/20-gpg.sh
+++ modules/gnupg/XDG_CONFIG_HOME.gnupg/rc.d/20-gpg.sh
@@ -5,5 +5,5 @@ export GPG_TTY
 
 gpg-connect-agent updatestartuptty /bye > /dev/null 2> /dev/null
 
-SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-export SSH_AUTH_SOCK
+# SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+# export SSH_AUTH_SOCK
