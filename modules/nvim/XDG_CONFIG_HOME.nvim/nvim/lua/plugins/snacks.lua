e9ee30d90245b549451180ae0da6dc91626b1fee
AlexAegis <alexaegis@pm.me>
2025-12-26 11:31:01 +0100

    chore: nvim, ssh config update

--- modules/nvim/XDG_CONFIG_HOME.nvim/nvim/lua/plugins/snacks.lua
+++ modules/nvim/XDG_CONFIG_HOME.nvim/nvim/lua/plugins/snacks.lua
@@ -1,37 +1,36 @@
 return {
-  {
-    "folke/snacks.nvim",
-    init = function()
-      vim.g.snacks_animate = false
-    end,
-    opts = {
-      dashboard = { enabled = false },
-      picker = {
-        hidden = true,
-        sources = {
-          -- These define their own options, we must override their defaults.
-          files = { hidden = true },
-          buffers = { hidden = true },
-          -- Explorer and the rest of the sources don't define their own opts
-          -- so it will use the picker options defined above and we can choose
-          -- to override them if desired.
-          explorer = {
-            ignored = true,
-            -- If you prefer a right aligned explorer, uncomment this:
-            -- layout = {
-            --   layout = {
-            --     position = "right",
-            --   },
-            -- },
-          },
-        },
-      },
-      terminal = {
-        win = {
-          position = "float",
-          border = "rounded",
-        },
-      },
-    },
-  },
+	{
+		"folke/snacks.nvim",
+		init = function()
+			vim.g.snacks_animate = false
+		end,
+		opts = {
+			dashboard = { enabled = false },
+			picker = {
+				hidden = true,
+				sources = {
+					-- These define their own options, we must override their defaults.
+					files = { hidden = true },
+					buffers = { hidden = true },
+					-- Explorer and the rest of the sources don't define their own opts
+					-- so it will use the picker options defined above and we can choose
+					-- to override them if desired.
+					explorer = {
+						ignored = true,
+						layout = {
+							layout = {
+								position = "right",
+							},
+						},
+					},
+				},
+			},
+			terminal = {
+				win = {
+					position = "float",
+					border = "rounded",
+				},
+			},
+		},
+	},
 }
--- modules/nvim/XDG_CONFIG_HOME.nvim/nvim/lua/plugins/treesitter.lua
+++ modules/nvim/XDG_CONFIG_HOME.nvim/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,22 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "bezhermoso/tree-sitter-ghostty",
-    },
-    opts_extend = { "ensure_installed" },
-    opts = {
-      -- Add custom treesitters not present by default in LazyVim.
-      ensure_installed = {
-        "csv",
-        "dockerfile",
-        "elixir",
-        "embedded_template",
-        "ghostty",
-        "go",
-        "heex",
-        "helm",
-        "htmldjango",
-        "ini",
-        "kdl",
-        "liquid",
-        "nginx",
-        "ron",
-        "ruby",
-        "terraform",
-        "toml",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"bezhermoso/tree-sitter-ghostty",
+		},
+		opts_extend = { "ensure_installed" },
+		opts = {
+			-- Add custom treesitters not present by default in LazyVim.
+			ensure_installed = {
+				"csv",
+				"dockerfile",
+				"embedded_template",
+				"ghostty",
+				"ini",
+				"rust",
+				"typescript",
+				"toml",
+			},
+		},
+	},
 }
